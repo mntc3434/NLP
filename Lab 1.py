849e75adbc3f785d3bb09762bf5e051a694bcf25
Mintesinot getu <148215622+mntc3434@users.noreply.github.com>
2025-04-03 22:58:11 +0300

    Add files via upload

---
+++ Lab 1.py
@@ -0,0 +1,20 @@
+import re
+#re.split
+text = "This... is a test, short and sweet, of split()."
+result = re.split(r'\W+', text)  # Split on one or more non-word characters
+
+print("R.SPLIT:",result)
+import re
+
+#re.sub
+text = "blue socks and red shoes"
+result = re.sub(r'blue|white|red', 'black', text)
+
+print("R.SUB:",result)
+import re
+
+#re.findall
+text = "12 dogs, 11 cats, 1 egg"
+result = re.findall(r'\d+', text)  # Use r'\d+' to match one or more digits
+
+print("R.FINDALL:",result)
