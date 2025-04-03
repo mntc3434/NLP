be023d555abcc62c74f9db3a1dd469f49e29d305
Mintesinot getu <148215622+mntc3434@users.noreply.github.com>
2025-04-03 22:58:50 +0300

    Add files via upload

---
+++ Lan 3.py
@@ -0,0 +1,5 @@
+import re
+text = """There are multiple ways we can perform tokenization on given text data. We can choose any method based on langauge, library and purpose of modeling."""
+# Split text by whitespace
+tokens = text.split()
+print(tokens)
