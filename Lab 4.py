b482d524b8d51081172883314e9e5d8024335807
Mintesinot getu <148215622+mntc3434@users.noreply.github.com>
2025-04-03 22:59:15 +0300

    Add files via upload

---
+++ Lab 4.py
@@ -0,0 +1,22 @@
+#Sentence Tokenization
+text = """Founded in 2002, SpaceX’s mission is to enable humans to become a spacefaring civilization and a multi-planet 
+species by building a self-sustaining city on Mars. In 2008, SpaceX’s Falcon 1 became the first privately developed 
+liquid-fuel launch vehicle to orbit the Earth."""
+# Splits at '.' 
+text.split('. ')
+#Regular Expression
+import re
+
+# Regular expression for sentence delimiters
+sentence_delimiter_pattern = r'[፡።.!?]'
+
+# Example usage
+text = "This is a sentence. እንኳን ወደ ዚህ ገጽ መጡ፡ How are you?"
+
+# Splitting text into sentences
+sentences = re.split(sentence_delimiter_pattern, text)
+
+# Removing any extra whitespace
+sentences = [sentence.strip() for sentence in sentences if sentence]
+
+print(sentences)
